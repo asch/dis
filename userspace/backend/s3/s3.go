8d6271a5847c146f7964f9c661aa9e901b95473c
Vojtech Aschenbrenner <v@asch.cz>
2020-10-28 13:06:45 +0100

    Add lru cache to s3 read path

--- userspace/backend/s3/s3.go
+++ userspace/backend/s3/s3.go
@@ -7,7 +7,9 @@ import (
 	"dis/extent"
 	"dis/parser"
 	"fmt"
+	"github.com/hashicorp/golang-lru/simplelru"
 	"sync"
+	"time"
 )
 
 const (
@@ -22,6 +24,7 @@ var (
 	s3op      *s3ops.S3session
 	s3m       *s3map.S3map
 	workloads chan *[]extent.Extent
+	lru       *simplelru.LRU
 )
 
 type S3Backend struct{}
@@ -45,11 +48,17 @@ func (this *S3Backend) Init() {
 	s3op = s3ops.New(&s3ops.Options{Bucket: bucket, Region: region, Remote: remote})
 	s3m = s3map.New()
 
+	var err error
+	lru, err = simplelru.NewLRU(200, nil)
+	if err != nil {
+		panic(err)
+	}
+
 	workloads = make(chan *[]extent.Extent, workloadsBuf)
 	go this.writer()
 }
 
-const s3limit = 1024 * 1024 * 128
+const s3limit = 1024 * 1024 * 32
 
 func (this *S3Backend) writer() {
 	var reads sync.WaitGroup
@@ -101,6 +110,42 @@ func (this *S3Backend) Write(extents *[]extent.Extent) {
 	workloads <- extents
 }
 
+var mutex sync.Mutex
+
+func cachedDownload(s3e *s3map.S3extent, slice *[]byte) {
+again:
+	mutex.Lock()
+
+	from := s3e.PBA * 512
+	to := (s3e.PBA + s3e.Len) * 512
+
+	if obj, ok := lru.Get(s3e.Key); ok && obj != nil {
+		copy(*slice, (*obj.(*[]byte))[from:to])
+	} else if ok && obj == nil {
+		mutex.Unlock()
+		time.Sleep(100 * time.Microsecond)
+		goto again
+	} else {
+		lru.Add(s3e.Key, nil)
+		mutex.Unlock()
+		buf := make([]byte, s3limit)
+		rng := "0-"
+		s3op.Download(s3e.Key, &buf, &rng)
+		copy(*slice, buf[from:to])
+		mutex.Lock()
+		lru.Add(s3e.Key, &buf)
+	}
+
+	mutex.Unlock()
+}
+
+func partDownload(s3e *s3map.S3extent, slice *[]byte) {
+	from := fmt.Sprintf("%d", s3e.PBA*512)
+	to := fmt.Sprintf("%d", (s3e.PBA+s3e.Len)*512-1)
+	rng := "bytes=" + from + "-" + to
+	s3op.Download(s3e.Key, slice, &rng)
+}
+
 func (this *S3Backend) Read(extents *[]extent.Extent) {
 	var reads sync.WaitGroup
 
@@ -116,10 +161,8 @@ func (this *S3Backend) Read(extents *[]extent.Extent) {
 					go func(s3e *s3map.S3extent, e *extent.Extent) {
 						s := (s3e.LBA - e.LBA) * 512
 						slice := buf[s:]
-						from := fmt.Sprintf("%d", s3e.PBA*512)
-						to := fmt.Sprintf("%d", (s3e.PBA+s3e.Len)*512-1)
-						rng := "bytes=" + from + "-" + to
-						s3op.Download(s3e.Key, &slice, &rng)
+						//partDownload(s3e, &slice)
+						cachedDownload(s3e, &slice)
 						s3reads.Done()
 					}(s3e, e)
 				}
--- userspace/go.mod
+++ userspace/go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/aws/aws-sdk-go v1.35.8
+	github.com/hashicorp/golang-lru v0.5.1
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.7.1
 	golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211
