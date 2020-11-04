e0411c48ce9bb702149ba9d5414e84e86af5813c
Vojtech Aschenbrenner <v@asch.cz>
2020-11-04 21:46:15 +0100

    Preallocate writelist in s3 write path to prevent reallocation during append

--- userspace/backend/s3/write.go
+++ userspace/backend/s3/write.go
@@ -47,7 +47,7 @@ func cacheReadWorker(jobs <-chan cacheReadJob) {
 
 func nextObject(key int64) (*[]byte, *[]*s3map.S3extent, int64, int64, *sync.WaitGroup) {
 	buf := make([]byte, 0, s3limit)
-	var writelist []*s3map.S3extent
+	writelist := make([]*s3map.S3extent, 0, 4096)
 	var blocks int64
 	var reads sync.WaitGroup
 
