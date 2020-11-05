8868e83bea4186292348be6f48c5b46800829c09
Vojtech Aschenbrenner <v@asch.cz>
2020-11-05 14:12:21 +0100

    Track space in write cache per single extent

--- userspace/backend/s3/write.go
+++ userspace/backend/s3/write.go
@@ -11,11 +11,12 @@ const workloadsBuf = 1024 * 1024 * 2
 const s3limit = 1024 * 1024 * 32
 
 const (
-	uploadWorkers    = 10
-	uploadBuf        = 10
-	cacheReadWorkers = 10
-	cacheReadBuf     = 10
-	mapUpdateBuf	 = uploadWorkers + uploadBuf
+	uploadWorkers     = 30
+	uploadBuf         = 10
+	cacheReadWorkers  = 30
+	cacheReadBuf      = 10
+	mapUpdateBuf      = uploadWorkers + uploadBuf
+	cacheWriteFreeBuf = 1024
 )
 
 type uploadJob struct {
@@ -25,10 +26,10 @@ type uploadJob struct {
 }
 
 type cacheReadJob struct {
-	pba          int64
-	buf          *[]byte
-	reads        *sync.WaitGroup
-	extentsRound *sync.WaitGroup
+	e                  *extent.Extent
+	buf                *[]byte
+	reads              *sync.WaitGroup
+	cacheWriteFreeChan chan *extent.Extent
 }
 
 func uploadWorker(jobs <-chan uploadJob) {
@@ -40,15 +41,21 @@ func uploadWorker(jobs <-chan uploadJob) {
 
 func cacheReadWorker(jobs <-chan cacheReadJob) {
 	for job := range jobs {
-		cache.Read(job.buf, job.pba*512)
+		cache.Read(job.buf, job.e.PBA*512)
+		job.cacheWriteFreeChan <- job.e
 		job.reads.Done()
-		job.extentsRound.Done()
 	}
 }
 
 func mapUpdateWorker(writelists <-chan *[]*s3map.S3extent) {
 	for writelist := range writelists {
 		s3m.Update(writelist)
+	}
+}
+
+func cacheWriteFree(cacheWriteFreeChan <-chan *extent.Extent) {
+	for e := range cacheWriteFreeChan {
+		cache.WriteFreeSingle(e)
 	}
 }
 
@@ -75,10 +82,11 @@ func writer() {
 	mapUpdateChan := make(chan *[]*s3map.S3extent, mapUpdateBuf)
 	go mapUpdateWorker(mapUpdateChan)
 
+	cacheWriteFreeChan := make(chan *extent.Extent, cacheWriteFreeBuf)
+	go cacheWriteFree(cacheWriteFreeChan)
+
 	buf, writelist, blocks, key, reads := nextObject(0)
 	for extents := range workloads {
-		extentsRound := new(sync.WaitGroup)
-		extentsRound.Add(len(*extents))
 		for i := range *extents {
 			e := &(*extents)[i]
 			if (blocks+e.Len)*512 > s3limit && len(*writelist) > 0 {
@@ -97,12 +105,8 @@ func writer() {
 
 			blocks += e.Len
 			reads.Add(1)
-			cacheReadChan <- cacheReadJob{e.PBA, &slice, reads, extentsRound}
+			cacheReadChan <- cacheReadJob{e, &slice, reads, cacheWriteFreeChan}
 		}
-		go func(e *[]extent.Extent) {
-			extentsRound.Wait()
-			cache.WriteFree(e)
-		}(extents)
 	}
 }
 
