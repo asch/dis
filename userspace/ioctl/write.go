e01d0de82ed14037da759b9007b90bf06870f0e5
Vojtech Aschenbrenner <v@asch.cz>
2020-11-07 12:35:08 +0100

    Add workaround when kernel sends zero-length extent array

--- userspace/ioctl/write.go
+++ userspace/ioctl/write.go
@@ -8,6 +8,11 @@ import (
 func Write() {
 	for {
 		extents := RWIOCTL(writeNo)
+		// FIXME: Probable bug in kernel code, sometimes zero-length ioctl set is being sent
+		if len(*extents) == 0 {
+			println("IOCTL: Zero-length extent set received from kernel!")
+			continue
+		}
 		cache.WriteTrack(extents)
 		backend.Write(extents)
 	}
