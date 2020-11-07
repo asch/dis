8d66a975fd22417a555ce740e9e53f9275bab04f
Vojtech Aschenbrenner <v@asch.cz>
2020-11-07 20:56:19 +0100

    Add cache prereader which reads one ioctl round at once

--- userspace/cache/cache.go
+++ userspace/cache/cache.go
@@ -102,3 +102,79 @@ func WriteUntrackMulti(e *[]*extent.Extent) {
 
 func roundDown(x, y int64) int64 { return x - x%y }
 func roundUp(x, y int64) int64   { return roundDown(x+y-1, y) }
+
+type Prereader struct {
+	buf  []byte
+	off1 int64
+	off2 int64
+}
+
+func NewPrereader(extents *[]extent.Extent) *Prereader {
+	prereader := new(Prereader)
+	var begin, end bool
+	for i := range *extents {
+		e := &(*extents)[i]
+		if e.PBA == 0+headerSectors {
+			begin = true
+		}
+		if e.PBA > 2*Base/3 {
+			end = true
+		}
+	}
+	wrapped := begin && end
+
+	if wrapped {
+		minL := int64(0)
+		maxL := minL
+		minR := Base
+		maxR := int64(math.MinInt64)
+		for i := range *extents {
+			e := &(*extents)[i]
+			if e.PBA < maxL || math.Abs(float64(e.PBA-maxL)) < math.Abs(float64(e.PBA+e.Len-minR)) {
+				if e.PBA+e.Len > maxL {
+					maxL = e.PBA + e.Len
+				}
+			} else {
+				if e.PBA < minR {
+					minR = e.PBA
+				}
+				if e.PBA+e.Len > maxR {
+					maxR = e.PBA + e.Len
+				}
+			}
+		}
+		prereader.buf = make([]byte, (maxL-minL+maxR-minR)*512)
+		prereader.off1 = minL
+		prereader.off2 = minR
+		bufL := prereader.buf[:(maxL-minL)*512]
+		bufR := prereader.buf[(maxR-minR)*512:]
+		Read(&bufL, minL*512)
+		Read(&bufR, minR*512)
+	} else {
+		var min, max int64 = math.MaxInt64, math.MinInt64
+		for i := range *extents {
+			e := &(*extents)[i]
+			if e.PBA < min {
+				min = e.PBA
+			}
+			if e.PBA+e.Len > max {
+				max = e.PBA + e.Len
+			}
+		}
+		prereader.buf = make([]byte, (max-min)*512)
+		prereader.off1 = min
+		prereader.off2 = 0
+		Read(&prereader.buf, min*512)
+	}
+
+	return prereader
+}
+
+func (this *Prereader) Copy(buf []byte, dest int64) {
+	if this.off2 == 0 || dest < this.off2 {
+		dest -= this.off1 * 512
+	} else {
+		dest -= this.off2 * 512
+	}
+	copy(buf, this.buf[dest:])
+}
