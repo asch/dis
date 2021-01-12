a8e07f75c2ba0de489572b770bf75b1b1ec99db3
Vojtech Aschenbrenner <v@asch.cz>
2021-01-12 19:22:51 +0100

    Add option to skip read during write in null backend

--- userspace/backend/null/null.go
+++ userspace/backend/null/null.go
@@ -2,16 +2,35 @@ package null
 
 import (
 	"dis/extent"
+	"dis/parser"
 	"fmt"
+)
+
+const (
+	configSection = "backend.null"
+	envPrefix     = "dis_backend_null"
+)
+
+var (
+	skipReadInWritePath bool
 )
 
 type NullBackend struct{}
 
 func (this *NullBackend) Init() {
+	v := parser.Sub(configSection)
+	v.SetEnvPrefix(envPrefix)
+
+	v.BindEnv("skipReadInWritePath")
+	skipReadInWritePath = v.GetBool("skipReadInWritePath")
+
 	fmt.Println("NullBackend.Init()")
 }
 
 func (this *NullBackend) Write(extents *[]extent.Extent) {
+	if skipReadInWritePath {
+		return
+	}
 	fmt.Println("NullBackend.Write()")
 }
 
