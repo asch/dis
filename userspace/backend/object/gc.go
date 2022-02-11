72e99d80b13f8766c317e910051eb048eaf974cc
Vojtech Aschenbrenner <v@asch.cz>
2022-02-11 23:24:58 +0100

    Add untested gcthread2
    gcthread2 is another garbage collection function which does not use
    range reads to get live data from garbage collected objects. Instead, it
    downloads complete objects into temporary buffer and read extents from
    there. This should speed up cases when multiple range reads are done on
    one object.

--- userspace/backend/object/gc.go
+++ userspace/backend/object/gc.go
@@ -128,3 +128,117 @@ func gcthread() {
 		fmt.Println("GC Done")
 	}
 }
+
+func gcthread2() {
+	if gcMode != "on" && gcMode != "silent" {
+		return
+	}
+	const gcPeriod = 5 * time.Second
+	for {
+		time.Sleep(gcPeriod)
+		if !gc.Needed() {
+			continue
+		}
+		gc.Running.Lock()
+		em.RLock()
+
+		fmt.Println("GC Started")
+		purgeSet := gc.GetPurgeSetGreedy()
+		fmt.Println("Objects viable for GC: ", len(*purgeSet))
+
+		gc.Running.Unlock()
+		em.RUnlock()
+
+		downloader := getDownloadChan()
+
+		// Buffer for downloaded objects
+		var buffer map[int64][]byte
+
+		// WaitGroup for running downloads
+		var wg sync.WaitGroup
+
+		for k, _ := range *purgeSet {
+			// Object
+			b := make([]byte, objectSize)
+
+			// Extent for the whole object
+			e := extmap.Extent{
+				LBA: 0,
+				PBA: 0,
+				Len: objectSize / 512,
+				Key: k,
+			}
+
+			wg.Add(1)
+			downloader <- downloadJob{&e, &b, &wg}
+
+			// Store object to buffer.
+			//
+			// Does not need to wait for download, just slice is
+			// copied and waiting is done after the end of the loop.
+			buffer[k] = b
+		}
+		wg.Wait()
+
+		gc.Running.Lock()
+		em.RLock()
+
+		wl := em.GenerateWritelist(purgeSet)
+		newPBAs := make([]int64, len(*wl))
+		newKeys := make([]int64, len(*wl))
+		slicedKeys := newKeys
+
+		uploader, uploadsWG := getUploadChan()
+
+		o := nextObject(true)
+		upload := func() {
+			if o.extents == 0 {
+				return
+			}
+			o.assignKey()
+
+			var i int64
+			for i = 0; i < o.extents; i++ {
+				slicedKeys[i] = o.key
+			}
+			slicedKeys = slicedKeys[i:]
+
+			uploadsWG.Add(1)
+			uploader <- o
+			o = nextObject(true)
+		}
+
+		for i, e := range *wl {
+			if o.size()+e.Len*512 > objectSize {
+				upload()
+			}
+
+			newPBAs[i] = o.blocks
+			slice := o.add(e.LBA, e.Len, true)
+
+			// Just copy needed extents from buffered objects
+			copy(slice, buffer[e.Key][e.PBA*512:(e.PBA+e.Len)*512])
+		}
+		upload()
+
+		uploadsWG.Wait()
+		em.RUnlock()
+		em.Lock()
+
+		for i, e := range *wl {
+			e.PBA = newPBAs[i]
+			e.Key = newKeys[i]
+			gc.Add(e.Key, e.Len)
+		}
+
+		em.Unlock()
+		gc.Running.Unlock()
+
+		for key := range *purgeSet {
+			s3.Void(key)
+			gc.Destroy(key)
+		}
+
+		fmt.Println("GC Done")
+	}
+}
